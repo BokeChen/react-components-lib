6a3c88caac146f87bb8cdc06b42ba72cb0582c5c
BokeChen <cbboke@aliyun.com>
2020-05-07 09:52:00 +0800

    feat: 增加左侧菜单和中间的内容展示区

--- src/common-parts/header/index.tsx
+++ src/common-parts/header/index.tsx
@@ -1,12 +1,12 @@
+import { Layout } from 'antd';
 import React from 'react';
 import styles from './assets/styles/index.module.scss';
 import headerUIObject from './headerUi';
-
 // interface Props {
 //   [key: string]: any;
 // }
-
+const { Header } = Layout;
 // 业务逻辑组件
 export default function CommonHeader() {
-  return <div className={styles.wrap}>{headerUIObject.renderFunction(styles)}</div>;
+  return <Header className={styles.wrap}>{headerUIObject.renderFunction(styles)}</Header>;
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,11 @@
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
 import CommonHeader from '@common-parts/header';
+import { Col, Layout, Menu, Row } from 'antd';
 import React, { useReducer } from 'react';
 import styles from './assets/styles/home.module.css';
 
+const { Content, Footer } = Layout;
+const { SubMenu } = Menu;
 const initialCount = { count: 0 };
 function reducer(state: { count: number }, action: { type: string; payload?: number }) {
   switch (action.type) {
@@ -19,9 +23,89 @@ export default function Home() {
   return (
       <>
           <CommonHeader />
-          <button type="button" className={styles.wrap} onClick={() => dispatch({ type: 'increment' })}>
-              home {state.count}
-          </button>
+          <Layout style={{ height: 'calc(100vh - 64px)' }}>
+              <Row style={{ height: '100%' }}>
+                  <Col span={6}>
+                      <div className="logo" />
+                      <Menu
+                          mode="inline"
+                          defaultSelectedKeys={['1']}
+                          defaultOpenKeys={['sub1']}
+                          style={{ height: '100%', borderRight: 0 }}
+            >
+                          <SubMenu
+                              key="sub1"
+                              title={
+                                  <span>
+                                      <UserOutlined />
+                                      subnav 1
+                                  </span>
+                }
+              >
+                              <Menu.Item key="1">option1</Menu.Item>
+                              <Menu.Item key="2">option2</Menu.Item>
+                              <Menu.Item key="3">option3</Menu.Item>
+                              <Menu.Item key="4">option4</Menu.Item>
+                          </SubMenu>
+                          <SubMenu
+                              key="sub2"
+                              title={
+                                  <span>
+                                      <LaptopOutlined />
+                                      subnav 2
+                                  </span>
+                }
+              >
+                              <Menu.Item key="5">option5</Menu.Item>
+                              <Menu.Item key="6">option6</Menu.Item>
+                              <Menu.Item key="7">option7</Menu.Item>
+                              <Menu.Item key="8">option8</Menu.Item>
+                          </SubMenu>
+                          <SubMenu
+                              key="sub3"
+                              title={
+                                  <span>
+                                      <NotificationOutlined />
+                                      subnav 3
+                                  </span>
+                }
+              >
+                              <Menu.Item key="9">option9</Menu.Item>
+                              <Menu.Item key="10">option10</Menu.Item>
+                              <Menu.Item key="11">option11</Menu.Item>
+                              <Menu.Item key="12">option12</Menu.Item>
+                          </SubMenu>
+                      </Menu>
+                  </Col>
+                  <Col span={18}>
+                      <Layout className="site-layout" style={{ marginLeft: 200 }}>
+                          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                              <div
+                                  className="site-layout-background"
+                                  style={{ padding: 24, textAlign: 'center' }}
+                >
+                                  ...
+                                  <br />
+                                  Really
+                                  <br />
+                                  ...
+                                  <br />
+                                  ... content
+                                  <br />
+                                  <button
+                                      type="button"
+                                      className={styles.wrap}
+                                      onClick={() => dispatch({ type: 'increment' })}
+                  >
+                                      home {state.count}
+                                  </button>
+                              </div>
+                          </Content>
+                          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                      </Layout>
+                  </Col>
+              </Row>
+          </Layout>
       </>
   );
 }
